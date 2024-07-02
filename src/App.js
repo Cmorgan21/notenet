c8f1116571fb170d293b754ea5cc0525b9beab4c
Callum <callummorgan666@gmail.com>
2024-07-02 16:49:57 +0100

    add configured routes in app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
-
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ProtectedRoute from "./components/ProtectedRoutes";
+import Home from "./pages/Home";
+import Header from "./components/Header";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import Notes from "./pages/Notes";
+import NoteDetail from "./components/NoteDetail";
+import Profile from "./pages/Profile";
 
 function App() {
   return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} exact="true" />
+        <Route path="/signin" element={<Signin />} exact="true" />
+        <Route
+          path="/notes"
+          element={
+            <ProtectedRoute>
+              <Notes />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/profile" element={<Profile />} />
 
-
+        <Route path="/signup" element={<Signup />} exact="true" />
+        <Route path="/notes/:id" element={<NoteDetail />} />
+      </Routes>
+    </Router>
   );
 }
 
