8dc23fa747f675a7e93833a8264963391c489611
Callum <callummorgan666@gmail.com>
2024-07-02 16:32:10 +0100

    add api configuration with tokens

---
+++ src/api.js
@@ -0,0 +1,38 @@
+import axios from "axios";
+import { ACCESS_TOKEN } from "./constants";
+
+const api = axios.create({
+  baseURL: "http://localhost:8000",
+});
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem(ACCESS_TOKEN);
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export const handleUnauthorized = () => {
+  localStorage.removeItem(ACCESS_TOKEN);
+  window.location.replace("/signin");
+};
+
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      handleUnauthorized();
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default api;
