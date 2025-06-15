71719aea2e2d5a47f796ced06b2162e5316d720f
Callum <callummorgan666@gmail.com>
2025-06-15 11:25:42 +0100

    add variables and states

--- src/pages/Categories.js
+++ src/pages/Categories.js
@@ -2,3 +2,15 @@ import { useEffect, useState } from "react";
 import api from "../api";
 import Loading from "../components/Loading";
 import SuccessMessage from "../components/SuccessMessage";
+
+export default function Categories() {
+  const [categories, setCategories] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [message, setMessage] = useState({ text: "", type: "" });
+
+  const [newCategory, setNewCategory] = useState({
+    name: "",
+    description: "",
+    color: "#ffffff",
+  });
+}
